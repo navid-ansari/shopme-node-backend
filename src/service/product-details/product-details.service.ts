1188844c26406dce6f40df7da81c9c6f2af4166f
navid-ansari <navidansari25july@gmail.com>
2023-08-25 02:01:09 +0530

    product detail service err res modfied

--- src/service/product-details/product-details.service.ts
+++ src/service/product-details/product-details.service.ts
@@ -8,17 +8,24 @@ import { errorResponse } from '../../lib/errors'
 
 export const productDetailsService = async (req: Request, res: Response) => {
   const id = req.params.id
+  const ObjectId = require('mongodb').ObjectId
   try {
     const product: IProduct = await ProductDetailsSchema.findById(id)
+    //const product: IProduct = await ProductDetailsSchema.findOne({ _id: id })
+    /*const product: IProduct = await ProductDetailsSchema.findOne({
+      _id: ObjectId(id)
+    })*/
+    //console.log(product)
     if (product) {
       return res.status(200).json(product)
     }
-    if (!product) {
+    /*if (!product) {
       return res.status(404).json({ message: 'Product not found' })
-    }
+    }*/
   } catch (error: any) {
     log.error(`Error in Product Details Service: ${error}`)
     //errorResponse(res, error)
-    return res.status(500).end()
+    //return res.status(500).end()
+    return res.status(404).json({ message: 'Product not found' })
   }
 }
